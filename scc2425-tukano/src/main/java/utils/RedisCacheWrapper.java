32db34eccdaf9695dae0e72ec138732e130210f8
Gui <guilherme.gomes.franco@gmail.com>
2024-12-09 21:12:09 +0000

    final changes

--- scc2425-tukano/src/main/java/utils/RedisCacheWrapper.java
+++ scc2425-tukano/src/main/java/utils/RedisCacheWrapper.java
@@ -5,20 +5,21 @@ import tukano.impl.rest.Session;
 public class RedisCacheWrapper {
 
 	private static RedisCacheWrapper instance;
+
 	synchronized public static RedisCacheWrapper getInstance() {
-		if(instance == null )
+		if (instance == null)
 			instance = new RedisCacheWrapper();
 		return instance;
 	}
-	
+
 	public void putSession(Session s) {
 		try (var jedis = RedisCache.getCachePool().getResource()) {
-			jedis.setex(s.uid(), 60, JSON.encode(s));
+			jedis.setex(s.uid(), 120, JSON.encode(s));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Session getSession(String uid) {
 		try (var jedis = RedisCache.getCachePool().getResource()) {
 			return JSON.decode(jedis.get(uid), Session.class);
