035cff2c3c725291dedb51187ca7450de4ebf690
oliveira <jmlcoliveira@gmail.com>
2024-12-13 18:33:47 +0000

    added yaml of http trigger

--- kubernets_yaml/commands.sh
+++ kubernets_yaml/commands.sh
@@ -1,18 +1,16 @@
-kubectl delete -n default deployment scc2425-webapp postgres redis
+kubectl delete -n default deployment scc2425-webapp postgres redis blob-http-trigger
 kubectl delete pvc postgres-pvc redis-pvc media-pvc
 kubectl delete -n default configmap db-init-scripts
 kubectl delete -n default secret db-user-secret
-kubectl delete -n default service scc2425-webapp-service postgres redis kubernetes
+kubectl delete -n default service scc2425-webapp-service postgres redis kubernetes blob-http-trigger
 kubectl delete persistentvolume media-pv postgres-pv redis-pv
 
 kubectl create secret generic db-user-secret \
   --from-literal=DB_USER=admin \
   --from-literal=DB_PASSWORD=AdminPassword \
   --from-literal=POSTGRES_USER=citus \
-  --from-literal=POSTGRES_PASSWORD=Admin1234
-
-#chmod +x init_user.sh
-#kubectl create configmap db-init-scripts --from-file=init_user.sh
+  --from-literal=POSTGRES_PASSWORD=Admin1234 \
+  --from-literal=POSTGRES_URL=jdbc:postgresql://postgres:5432/postgres-scc-2425 \
 
 kubectl apply -f postgres-pvc.yaml
 kubectl apply -f postgres.yaml
@@ -28,6 +26,8 @@ kubectl wait --for=condition=ready pod -l app=postgres
 kubectl wait --for=condition=ready pod -l app=redis
 
 kubectl apply -f webapp.yaml
+
+kubectl apply -f blob-http-trigger.yaml
 
 kubectl wait --for=condition=ready pod -l app=scc2425-webapp
 
